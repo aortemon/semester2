639f6713e03015159956d1d07ace5a93ada7ebde
Artem Z <67142290+aortemon@users.noreply.github.com>
2023-05-25 07:29:20 +0300

    Initial

---
+++ Matrix.cpp
@@ -0,0 +1,146 @@
+﻿// Разработать класс Матрица.
+// Класс должен хранить квадратную матрицу целых чисел заданного размера(от 2 до 8 строк). Класс должен предоставлять следующие операции:
+// 1) задать размер матрицы,
+// 2) узнать размер матрицы,
+// 3) задать элемент матрицы по его индексам,
+// 4) узнать элемент матрицы по его индексам,
+// 5) проверить, обладает ли матрица диагональным преобладанием,
+// 6) вычислить сумму двух матриц одного размера.
+// Класс должен содержать все необходимые конструкторы, деструктор, оператор присваивания, а также «уметь» выводить себя на консоль.
+
+
+#include <iostream>
+using namespace std;
+
+
+class Matrix {
+	private:
+		int size;
+		int** nums;
+	public:
+		Matrix(int n = 3) {
+			size = n;
+			nums = new int*[n];
+			for (int i = 0; i < size; i++) 
+				nums[i] = new int[n];
+			for (int i = 0; i < size; i++) 
+				for (int j = 0; j < size; j++)
+					nums[i][j] = 0;
+		}
+
+		Matrix(const Matrix& matr) {
+			size = matr.size;
+			nums = new int* [size];
+			for (int i = 0; i < size; i++) {
+				nums[i] = new int[size];
+				for (int j = 0; j < size; j++)
+					nums[i][j] = matr.nums[i][j];
+			}
+		}
+
+
+		~Matrix() {
+			for (int i = 0; i < size; i++) 
+				delete[] nums[i];
+			delete[] nums;
+		}
+
+		void set_size(int n) {
+			size = n;
+			nums = new int* [n];
+			for (int i = 0; i < size; i++)
+				nums[i] = new int[n];
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++)
+					nums[i][j] = 0;
+		}
+
+		int get_size() const {
+			return size;
+		}
+
+		int* operator[](int i) {
+			return nums[i];
+		}
+
+		bool is_DDM() const {
+			bool is_strong = false;
+			for (int i = 0; i < size; i++) {
+				int amount = 0;
+				for (int j = 0; j < size; j++) {
+					if (i != j) amount += nums[i][j];
+				}
+				if (nums[i][i] < amount) return false;
+				else if (nums[i][i] > amount) is_strong = true;
+			}
+			return true && is_strong;
+		}
+
+		Matrix operator+(const Matrix& add) {
+			if (size != add.size) throw invalid_argument("Не умею складывать матрицы разных размеров!");
+
+			Matrix result(size);
+
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++) 
+					result.nums[i][j] = nums[i][j] + add.nums[i][j];
+			return result;
+		}
+
+		Matrix& operator=(const Matrix& mtrx) {
+			size = mtrx.size;
+			nums = new int* [size];
+			for (int i = 0; i < size; i++) {
+				nums[i] = new int[size];
+				for (int j = 0; j < size; j++)
+					nums[i][j] = mtrx.nums[i][j];
+			}
+		}
+
+		friend ostream& operator<<(ostream& os, const Matrix& mtrx) {
+			for (int i = 0; i < mtrx.size; i++) {
+				for (int j = 0; j < mtrx.size; j++)
+					os << mtrx.nums[i][j] << "  ";
+				os << endl;
+			}
+			return os;
+		}
+};
+
+int main() {
+	Matrix A(2);
+	Matrix B(3);
+
+	A.set_size(3);
+
+	cout << "A Matrix Size: " << A.get_size() << endl;
+
+	for (int i = 0; i < 3; i++) 
+		for (int j = 0; j < 3; j++) {
+			A[i][j] = i * 3 + j;
+			B[i][j] = 9 - i * 3 - j;
+		}
+	
+
+	cout << "A Matrix:" << endl << A << endl;
+	cout << "B Matrix:" << endl << B << endl;
+	cout << "A + B Matrix: " << endl << A + B << endl;
+
+
+	Matrix C = A + B;
+
+	C[0][0] = 18;
+	C[1][1] = 18;
+	C[2][2] = 18;
+
+
+
+	cout << "C = A + B, C[x][x] = 18, x=1,2,3" << endl;
+	cout << C << endl;
+	cout << "Is C DD Matrix: " << C.is_DDM();
+
+
+
+
+	return 0;
+}
---
+++ Vector.cpp
@@ -0,0 +1,120 @@
+﻿// Разработать класс Вектор.
+// Класс должен хранить вектор целых чисел заданного размера(от 1 до 20).Класс должен предоставлять следующие операции :
+// 1) задать размер вектора,
+// 2) узнать размер вектора,
+// 3) задать компоненту вектора по ее номеру,
+// 4) узнать компоненту вектора по ее номеру,
+// 5) вычислить длину вектора,
+// 6) найти скалярное произведение двух векторов,
+// 7) найти сумму двух векторов одного размера.
+// Класс должен содержать все необходимые конструкторы, деструктор, оператор присваивания, а также «уметь» выводить себя на консоль.
+
+#include <iostream>
+#include <locale>
+using namespace std;
+
+class Vector {
+private:
+	int size;
+	int* nums;
+public:
+	Vector(int size_ = 1) {
+		size = size_;
+		nums = new int[size];
+		for (int i = 0; i < size; i++) nums[i] = 0;
+	}
+
+	Vector(const Vector& element) {
+		size = element.size;
+		nums = new int[size];
+		for (int i = 0; i < size; i++) {
+			nums[i] = element.nums[i];
+		}
+	}
+
+	~Vector() {
+		delete[] nums;
+	}
+
+	void set_size(int new_size) {
+		size = new_size;
+		nums = new int[size];
+		for (int i = 0; i < size; i++) nums[i] = 0;
+	}
+
+	int get_size() const {
+		return size;
+	}
+
+	int& operator[](int i) {
+		return nums[i];
+	}
+
+	double get_length() {
+		int amount = 0;
+		for (int i = 0; i < size; i++) {
+			amount += nums[i] * nums[i];
+		}
+		return sqrt(amount);
+	}
+
+	const Vector operator+(const Vector& add) {
+		if (size != add.size) throw invalid_argument("Не могу сложить векторы разных размеров!");
+		Vector result(size);
+		for (int i = 0; i < size; i++) {
+			result[i] = nums[i] + add.nums[i];
+		}
+		return result;
+	}
+
+	const double operator*(const Vector& mult) const {
+		if (this->size != mult.size) throw invalid_argument("Не могу умножить векторы разных размеров!");
+		double result = 0;
+		for (int i = 0; i < size; i++) {
+			result += mult.nums[i] * nums[i];
+		}
+		return result;
+	}
+
+	Vector& operator=(const Vector& equ) {
+		size = equ.size;
+		nums = new int[size];
+		for (int i = 0; i < size; i++) 
+			nums[i] = equ.nums[i];
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream& os, const Vector& vect) {
+		os << "{  ";
+		for (int i = 0; i < vect.size; i++)
+			os << vect.nums[i] << "  ";
+		os << "}";
+		return os;
+	}
+
+};
+
+int main() {
+	setlocale(LC_CTYPE, "ru");
+	Vector a(2), b(2);
+	a.set_size(5);
+	for (int i = 0; i < 5; i++) a[i] = (i + 1) * (i + 1);
+	cout << "Vector Size: " << a.get_size() << endl;
+	cout << "a[2] = " << a[2] << endl;
+	a[3] = 12345678;
+	cout << "a[3] = " << a[3] << endl;
+	a.set_size(2);
+	a[0] = 4;
+	a[1] = 3;
+	b[0] = 1;
+	b[1] = -1;
+	cout << "Vector Length: " << a.get_length() << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " + " << b << " = " << (a + b) << endl;
+	a = b;
+	cout << a << endl << b;
+
+
+
+	return 0;
+}
